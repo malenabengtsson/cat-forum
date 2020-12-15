3ca9705070c98c3492ea1d5bccff869bb3c2197c
Malena Bengtsson <malena94bengtsson@gmail.com>
2020-12-15 12:26:08 +0100

    Added react and started configuration server

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const session = require('express-session');
+const store = require('better-express-store');
+const app = express();
+const RestApi = require('./RestApi')
+
+app.use(express.json());
+
+app.use(session({
+  secret: require('./session-secret.json'),
+  resave: false,
+  saveUninitialized: true,
+  cookie: {secure: 'auto'},
+  store: store({dbPath: '../cat-forum.db'})
+}))
+
+app.listen(3000, () =>{
+  console.log('Listening on port 3000');
+})
+
+new RestApi(app);
